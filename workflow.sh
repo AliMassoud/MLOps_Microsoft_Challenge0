5fe5102a4aa89478fb41f7f5c5445482f8846271
AliMassoud <ali.masoud@lau.edu>
2023-04-07 15:51:15 +0200

    testing

--- workflow.sh
+++ workflow.sh
@@ -1,7 +1,8 @@
-az group create --name def_resource_group --location francecentral
+# az group create --name def_resource_group --location francecentral
+echo hey
 az configure defaults --group=def_resource_group
-az ml workflow create --name def-workflow
-az configure defaults --workflow=def-workflow
+az ml workspace create --name def-workspace
+az configure defaults --workspace=def-workspace
 # az data create --name diabetes-dev-folder --path /experimentation/data
 # az compute create --name def-cluster --size STANDARD_DS11_V2 --max-instances 2 --type AmlCompute
 # gh workflow run .github/workflows/02-manual-trigger-job.yml 
